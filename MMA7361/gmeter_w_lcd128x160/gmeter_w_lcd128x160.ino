0e2e1b79507141fac5445fc8214c22c731e76258
Artem Mamchych <amamch@softserveinc.com>
2014-05-13 21:15:25 +0300

    Improved tftPrintGraph() and insreased drawing speed

--- MMA7361/gmeter_w_lcd128x160/gmeter_w_lcd128x160.ino
+++ MMA7361/gmeter_w_lcd128x160/gmeter_w_lcd128x160.ino
@@ -1,16 +1,12 @@
-//LCD
-#define cs   10
-#define dc   9
-#define rst  8  // you can also connect this to the Arduino reset
-
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library
 #include <SPI.h>
 
-#if defined(__SAM3X8E__)
-    #undef __FlashStringHelper::F(string_literal)
-    #define F(string_literal) string_literal
-#endif
+//LCD pin cs   10
+//LCD pin dc   9
+//LCD pin rst  8
+//Using hardware SPI pins for max speed
+Adafruit_ST7735 tft = Adafruit_ST7735(); //Library was optimized for max speed, sources available here: https://github.com/Artem-Mamchych/Adafruit-ST7735-Library
 
 //MMA7361
 #define PIN_X     A0
@@ -19,17 +15,17 @@
 #define PIN_SLEEP A3
 //#define PIN_FREEFALL_DETECT 12
 
+#define FREEFALL_EVENT   HIGH
 #define SLEEP_MODE_ON    LOW
 #define SLEEP_MODE_OFF   HIGH
 //For lower power consumption, it is recommended to set g-Select to 1.5g mode.
-
-#define FREEFALL_EVENT   HIGH
 
 //#define PIN_SENSITIVITY_MODE 11 //Not used
 //g-Range Sensitivity:
 #define MODE_1.5G LOW  //+- 1.5g 800 mV/g
 #define MODE_6G   HIGH //+- 6g 206 mV/g
 
+//This calibration coefficients is specific for each MMA7361 chip
 #define CALIB_VAL_X 407
 #define CALIB_VAL_Y 427
 #define CALIB_VAL_Z 565
@@ -38,12 +34,7 @@ int x, y, z, accel;
 int prev_x, prev_y, prev_z, prev_accel;
 int width, height;
 int graph_pos = 0;
-uint16_t time1;
-uint16_t time2;
-
-// Use hardware SPI pins (for UNO thats sclk = 13 and sid = 11)
-//and pin 10 must be an output. This is much faster
-Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
+uint16_t lcdCleanTime, lcdFillTime;
 
 void setup(void) {
   pinMode(PIN_SLEEP, OUTPUT);
@@ -51,10 +42,10 @@ void setup(void) {
   digitalWrite(PIN_SLEEP, SLEEP_MODE_OFF);
   Serial.begin(9600);
   
-  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
+  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, LCD controller
   tft.fillScreen(ST7735_BLACK);
   tft.setRotation(3);
-  tft.setTextSize(2); //3
+  tft.setTextSize(2);
   width = tft.width();
   height = tft.height();
 }
@@ -63,19 +54,17 @@ void loop() {
   getAccelValues();
    
   //uartPrintValues()
+//  lcdCleanTime = millis();
   tftCleanText();
+//  lcdFillTime = millis();
   tftPrintValues();
-  
   tftPrintGraph();
-  //Refresh Rate
-  delay(80);
-//  delay(500);
-//25Hz = 40ms
-//10Hz = 100ms
   
-//  time1 = millis();
-//  clearLCD()
-//  Serial.println(millis() - time1); //LCD Cleaning time = 93ms
+//  Serial.print(lcdFillTime - lcdCleanTime); //clean time, ms
+//  Serial.print(" /");
+//  Serial.println(millis() - lcdFillTime); //draw time, ms 
+
+  delay(80); //Refresh Rate
 }
 
 void getAccelValues() {
@@ -94,7 +83,7 @@ void clearLCD() {
   tft.fillRect(34, 0, 130, 95, ST7735_BLACK);
 }
 
-void testdrawtext(char *text, uint16_t color) {
+void drawAlertMessage(char *text, uint16_t color) {
   tft.setCursor(0, 0);
   tft.setTextColor(color);
   tft.setTextWrap(true);
@@ -102,7 +91,6 @@ void testdrawtext(char *text, uint16_t color) {
 }
 
 void uartPrintValues() {
-  // Вывод в Serial monitor
   Serial.print(" X: ");
   Serial.print(x);
   Serial.print(" Y: ");
@@ -112,38 +100,39 @@ void uartPrintValues() {
 }
 
 void tftPrintGraph() {
-  if (graph_pos >= width) { //If at the edge of the window, go back to the beginning:
-    tft.fillScreen(ST7735_BLACK);
-    graph_pos = 0;
-  } else {
-    tft.drawPixel(graph_pos, x, ST7735_BLUE);
-    tft.drawPixel(graph_pos, y, ST7735_RED);
-    tft.drawPixel(graph_pos, accel, ST7735_WHITE);    
-//    tft.drawFastHLine(x, y, width, ST7735_RED);
-    ++graph_pos;
-  }
+//  tft.drawPixel(graph_pos, accel, ST7735_WHITE); 
+  tft.drawLine(graph_pos-1, (prev_accel+512)>>3, graph_pos, (accel+512)>>3, ST7735_YELLOW);
+      
+  tft.drawPixel(graph_pos, (x+512)>>3, ST7735_BLUE);
+  tft.drawPixel(graph_pos, (y+512)>>3, ST7735_RED);   
+  ++graph_pos;
 }
 
 void tftCleanText() {
   tft.setCursor(0, 0);
-  tft.setTextColor(ST7735_BLACK);
+  if (graph_pos >= width) { //If at the edge of the window, go back to the beginning:
+    tft.fillScreen(ST7735_BLACK);
+    graph_pos = 1;
+  } else {
+    tft.setTextColor(ST7735_BLACK);
+    
+    tft.print("X:");
+    tft.println(prev_x);
+    tft.print("Y:");
+    tft.println(prev_y);
+    tft.print("Z:");
+    tft.println(prev_z);
+    tft.print("Ac");
+    tft.println(prev_accel);
+  }  
   
-  tft.print("X:");
-  tft.println(prev_x);
-  tft.print("Y:");
-  tft.println(prev_y);
-  tft.print("Z:");
-  tft.println(prev_z);
-  tft.print("Ac");
-  tft.println(prev_accel);
+  //drawChar(uint16_t x, uint16_t y, char c, uint16_t color, uint16_t bg, uint8_t size);
 }
 
-void tftPrintValues() {
-//  time2 = millis();    
+void tftPrintValues() {  
   tft.setCursor(0, 0);
-//  tft.fillScreen(ST7735_BLACK);
 //  tft.fillRect(34, 0, 130, 95, ST7735_BLACK);
-//  tft.setTextSize(3);
+
   tft.setTextColor(ST7735_BLUE);
   tft.print("X:");
   tft.println(x);
@@ -156,8 +145,4 @@ void tftPrintValues() {
   tft.setTextColor(ST7735_YELLOW);
   tft.print("Ac");
   tft.println(accel);
-//  Serial.print("LCD ");
-
-//  Serial.println(millis() - time2); //Text printing time = 42ms
-//In v2 =29ms (without abs(x) + abs(y))
 }
